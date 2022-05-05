bb4eac57656778c79f715d4380c8d4216a5f71dd
Rafa Miranda <43553238+rafael749@users.noreply.github.com>
2022-05-05 18:15:49 -0300

    index.js
    Se sube el archivo index.js que contiene la primer entrega final de JavaScript

---
+++ index.js
@@ -0,0 +1,231 @@
+// ENTREGA Numero  1  y 2
+
+/*
+1)  Para este desafío simulo un carrito de compras, en el cual se debe loguar primero con la password correcta. -->  admin123
+        Si la misma se ingresa 3 veces mal, Al 4to intento se le infica al usuario que está bloqueado.
+        Debe presionar F5 para volver a intentar.
+2)  Si el usuario ingresa al sistema de forma correcta:
+        En caso de arrepentirse puede "salir". El sistema envia un mensaje indicando que no realizó ninguna compra. y finaliza el mismo
+        Si desea comprar en nuestra tienda debe escribir "comprar".
+        para comenzar seleccionando la categoría. o escribir "salir".
+        Si ingresa una categoría que no está en sistema, se le indica que vuelva a ingresarla.
+        Luego de seleccionar la categoria, continúa seleccionando el producto. 
+        Si ingresa un producto que  que no está en sistema, se le indica que vuelva a ingresarlo.
+3)  Si desea continuar comprando, debe escribir "comprar" -> Se repite el proceso 2)
+        Caso contrario "salir" para finalizar el programa.
+4)Al finalizar la compra se le indica al usuario, cuantos productos ha comprado
+Muchas Gracias.-
+*/
+
+
+/******************************************************************************************************/
+
+
+//          ENTREGA Numero 3  03/05/2022
+
+/*
+    Se Crean dos clases Categoría y Productos,
+    Se crean dos array categoria y producto con los objetos necesarios para la prueba
+*/
+
+
+
+/******************************************************************************************************/
+
+
+//         Primera entrega del Proyecto Final   05/05/2022
+
+
+		//MEJORA
+		
+/*		Dentro de cada eleccion del usuario (Categoria y producto),Se le añade como  indice la variable con el numero seleccionado Sin la necesidad de usar un bucle.
+		ANTES En la entrega anterior lo harcodeaba a mano el numero  */			
+
+
+class Categoria{
+    constructor(id,nombre,estado){
+        this.id = id;
+        this.nombre = nombre;
+        this.estado;
+    }
+}
+
+class producto{
+    constructor(id, nombre,stock,precio){
+        this.id = id;
+        this.nombre = nombre;
+        this.stock = stock;
+        this.precio = precio;
+    }  
+}
+
+const productos = [
+    {id:1001, nombre:'IPhone', stock:'50', precio: '500'},
+    {id:1002, nombre:'SmarTv', stock:'60', precio: '600'},
+    {id:1003, nombre:'Heladera', stock:'70', precio: '700'},
+    {id:1004, nombre:'Silla Gamer', stock:'80', precio: '800'},
+]
+
+const categorias = [
+    {id:1001, nombre:'Ofertas', estado:'disponible', productos},
+    {id:1002, nombre:'Tecnologias', estado:'isponible', productos},
+    {id:1003, nombre:'Electrodomesticos', estado:'isponible', productos},
+    {id:1004, nombre:'Accesorios', estado:'isponible', productos},
+]
+
+
+
+
+
+
+/*******************************************************************************************************************/
+//Carrito de Compras 
+/*******************************************************************************************************************/
+
+let carritoCompras = () => {
+    let comenzarAcomprar = prompt('Para comenzar a comprar en el sitio escriba: comprar -  Para finalizar programa escriba Salir. Muchas Gracias');
+
+    eleccionDelUsuario(comenzarAcomprar);
+}
+
+let ararayCarritoCategorias = new Array();
+let ararayCarritoProductos = new Array();
+
+/***********Decisión del usuario, Si decide comprar comienza seleccionando la Categoría********************************/
+
+let eleccionDelUsuario = comenzarAcomprar => {
+
+    if (comenzarAcomprar != 'comprar') {
+
+        alert('Lamentamos que te vayas sin realizar una compra, vuelve pronto');
+        alert('En caso que te hubieras equivocado, presiona F5 y escribe comprar. Muchas Gracias');
+
+    } else {
+
+        let carritoCategoria = [];
+        let carritoProducto = [];
+        let carritoProductoTotal = [];
+        let productosEnCanasta = 0;
+
+        while (comenzarAcomprar != 'salir') {
+
+            let carritoCompras = prompt("Seleccione categoria del producto a comprar: 0 para Ofertas - 1 para Tecnologías - 2 para electrodomesticos - 3 para accesorios ");
+            switch (carritoCompras) {
+                case "0":
+                    ararayCarritoCategorias.push(categorias[carritoCompras]);
+                    alert(`Categorias seleccionadas: ${categorias[carritoCompras].nombre}`);
+                    prod(categorias[carritoCompras].nombre);
+                    break;
+                case "1":
+                    ararayCarritoCategorias.push(categorias[carritoCompras]);
+                    alert(`Categorias seleccionadas: ${categorias[carritoCompras].nombre}`);
+                    prod(categorias[carritoCompras].nombre);
+                    break;
+                case "2":
+                    ararayCarritoCategorias.push(categorias[carritoCompras]);
+                    alert(`Categorias seleccionadas: ${categorias[carritoCompras].nombre}`);
+                    prod(categorias[carritoCompras].nombre);
+                    break;
+                case "3":
+                    ararayCarritoCategorias.push(categorias[carritoCompras]);
+                    alert(`Categorias seleccionadas: ${categorias[carritoCompras].nombre}`);
+                    prod(categorias[carritoCompras].nombre);
+                    break;
+                default:
+                    alert(`Lo siento no disponemos de ese producto ${carritoCompras}. Debes seleccionar correctamente del desde el 1 al 4`);
+                    break;
+            }
+
+            comenzarAcomprar = prompt('Presiona comprar para continuar comprando, Para finalizar programa escriba Salir. Muchas Gracias');
+
+        }
+
+        for(const myCarritoCategorias of ararayCarritoCategorias){
+
+            carritoCategoria.push(myCarritoCategorias.nombre);
+        }
+
+        for(const myCarritoProductos of ararayCarritoProductos){
+            carritoProducto.push(myCarritoProductos.nombre);
+
+            productosEnCanasta++;
+        }
+
+        alert(`Categorias que compró... \n${carritoCategoria}\n\nProductos:\n${carritoProducto}\n\nTotal de ${productosEnCanasta} Productos!`);
+    }
+}
+
+/*******************Continuamos la compra seleccionando el producto******************/
+
+let prod = prod => {
+
+    let producto = prompt("Ahora Elige el producto a comprar: 0 para Iphone - 1 para Smart-Tv - 2 para Heladera-Smart - 3 para Silla-Gamer ");
+    let produ = 0;
+    switch (producto) {
+        case "0":
+            ararayCarritoProductos.push(categorias[producto].productos[producto]);
+            alert('Seleccionaste Iphone precio $500');
+
+            produ = 500;
+            break;
+        case "1":
+            ararayCarritoProductos.push(categorias[producto].productos[producto]);
+            alert('Seleccionaste Smart-Tv precio $600');
+
+            produ = 600;
+            break;
+        case "2":
+            ararayCarritoProductos.push(categorias[producto].productos[producto]);
+            alert('Seleccionaste Heladera-Smart $700');
+
+            produ = 700;
+            break;
+        case "3":
+            ararayCarritoProductos.push(categorias[producto].productos[producto]);
+            alert('Seleccionaste Silla-Gamer $800');
+
+            produ = 800;
+            break;
+        default:
+            alert(`Lo siento no disponemos de ese producto ${producto}`);
+            break;
+    }
+    return produ;
+}
+
+                    /****************************************************/
+                                    //Inicio del programa
+                    /****************************************************/
+
+
+                    //LOGIN
+
+//Password a Ingresar: admin123
+
+let password = prompt('Ingresa tu password de Administrador para ingresar');
+
+    for (let intentos = 1; intentos < 5; intentos++) {
+
+        if (password != 'admin123') {
+
+            alert(`La password ${password} ingresada es incorrecta. Intento Nro: ${intentos}`);
+
+            password = prompt('Vuelve a ingresar tu password de Administrador para ingresar');        
+
+            if (intentos > 2) {
+                alert('bloqueaste tu cuenta. Por favor presiona F5 y vuelve a probar. sino vuelvas prontos');
+                break;
+            }
+        }else{
+
+            //Inicia el carrito de compras
+        
+            alert('Bienvenidos al mejor Ecommerce de CoderHouse');
+            carritoCompras();
+            break;
+        
+        }
+    }
+
+
+/**************** F I N del programa =) ************************************/
