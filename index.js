1ad3c3b4cda856b0eecc0add3e954ca95067bd03
DesarrolloWebEcommerce <rafaelmiranda749@gmail.com>
2022-05-22 20:04:40 -0300

    se crea por separado un script para cargar los productos al sistema

---
+++ index.js
@@ -0,0 +1,116 @@
+
+//MUESTRO LOS PRODUCTOS QUE TIENE EL CLIENTE AÑADIDOS AL CARRITO CARGADOS EN EL LOCALSTORAGE
+if(localStorage.listaProductosCarrito != null){	
+document.getElementById('idCarrito').innerText = JSON.parse(localStorage.listaProductosCarrito).length;
+}
+
+let carrito= [];
+
+let mostrarProductoCargadosEnSistema = (arr) => {
+
+    let contadorProd=0;
+
+    for (const el of arr){
+        if(el.categoriaProducto==="tecnologias" || el.categoriaProducto==="accesorios"){
+            let grillaPadre = document.getElementsByClassName('grillaPadre')
+            let producto = document.createElement("div");
+            producto.className = `producto${contadorProd} productos card__css-manual rounded`;
+            producto.innerHTML = 	`<span id="" data-action="cart-can" class="cart_menu cart_menu_Nuevo badge rounded">${el.estadoProducto}</span> 
+                                                <span id="" data-action="cart-can" class="cart_menu cart_menu_Envio badge rounded">${el.envioProducto}</span> 
+                                                <img src="${el.imagen}" alt="Producto Nuevo Agregado con Js"> 
+                                         <div class="productoDetalle"> 
+                                                <h3 class="precio__actual">$ ${el.precioProducto}</h3> 
+                                                <h5>${el.nombreProducto}</h5> 
+                                                <h6>${el.modeloProducto}</h6> 
+                                            </div> 
+                                            <div class="carrito"> 
+												<i id="${el.idProducto}" class="fas fa-heart heart" data-bs-toggle="tooltip" data-bs-placement="top" title="Agregar a Deseos" ></i>
+                                                <i class="fas fa-eye eye data-bs-toggle="tooltip" data-bs-placement="top" title="Ver Producto"></i>                                          
+                                                <i id="producto${el.idProducto}" class="fas fa-shopping-cart shopping-car agregarCarrito" data-bs-toggle="tooltip" data-bs-placement="top" title="Agregar al Carrito"></i>
+                                            </div>`
+            contadorProd++;
+            grillaPadre[0].appendChild(producto);
+
+
+			let btnListaDeseos = document.getElementById(`${el.idProducto}`);
+			btnListaDeseos.addEventListener("click",listaDeseos);
+
+			let btnListaCarrito = document.getElementById(`producto${el.idProducto}`);
+			btnListaCarrito.addEventListener('click',() => {agregarAlCarrito(el.idProducto)
+			});
+
+			if(contadorProd===10){break;}
+        } 	
+    }					
+}    
+
+mostrarProductoCargadosEnSistema(productos);
+
+
+//AGREGAR A LA LISTA DE DESEOS y COMPRA LOS PRODUCTOS SELECCIONADOS
+
+let misDeseos = parseInt(document.getElementById('idDeseos').innerText);
+let miCarrito = parseInt(document.getElementById('idCarrito').innerText);
+
+function listaDeseos(id){
+	console.log(`Producto sumado a la lista de Deseos ${id.target.id}`)
+	let agregarDeseo = ++misDeseos;
+
+	document.getElementById('idDeseos').innerText = agregarDeseo;
+
+	//TODO Crear la misma lógica para añadir al localstorage los deseos
+	swal({
+		title: "",
+		text: "¡Producto agregado a la lista de deseos!",
+		type: "success",
+		confirmButtonColor: "#59a501",
+		closeOnConfirm: false
+	});
+}
+
+const agregarAlCarrito = (idProd) => {
+
+	//AÑADIMOS EL PRODUCTO SELECCIONADO A CARRITO[]
+	const item = productos.find((prod) => prod.idProducto === idProd);
+	
+	//Si el cliente ya tiene compras en el carrito, se añaden el mismo. SINO en el array carrito[] vacío.
+	if(localStorage.listaProductosCarrito != null){
+	 	carrito = JSON.parse(localStorage.listaProductosCarrito);
+	};
+	carrito.push({
+		"categoriaProducto": item.categoriaProducto,
+		"nombreProducto":item.nombreProducto,
+		"marcaProducto":item.marcaProducto,
+		"precioProducto":item.precioProducto,
+		"modeloProducto":item.modeloProducto,
+		"envioProducto":item.envioProducto,
+		"estadoProducto":item.estadoProducto,
+		"imagen":item.imagen,
+		"idProducto":item.idProducto
+	});
+	
+	console.log("carrito",carrito)
+
+	//PASAMOS CARRITO AL LOCAL STORAGE
+
+	localStorage.setItem("listaProductosCarrito",JSON.stringify(carrito)); 	
+	document.getElementById('idCarrito').innerText = carrito.length
+	
+	//Mostrar alerta del producto Agregado
+	swal({
+		title: "",
+		text: "¡Se ha agregado un nuevo producto al carrito de compras!",
+		type: "success",
+		showCancelButton: true,
+		confirmButtonColor: "#DD6B55",
+		cancelButtonText: "¡Continuar comprando!",
+		confirmButtonText: "¡Ir a mi carrito de compras!",
+		closeOnConfirm: false
+	  },
+	  function(isConfirm){
+		  if (isConfirm) {
+			  window.location.href = "/html/pasarelaDePago.html"
+		 	} 
+  });
+}
+        
--- pasarelaDePago.js
+++ pasarelaDePago.js
@@ -1,28 +1,48 @@
-let listaCarrito = JSON.parse(localStorage.getItem("listaProductosCarrito"))
+//MUESTRO LOS PRODUCTOS QUE TIENE EL CLIENTE AÑADIDOS AL CARRITO CUANDO ESTOS PASAN AL LOCALSTORAGE
+if(localStorage.listaProductosCarrito != null){
 
-for(let productoEnCanasto of listaCarrito){
+    //MUESTRO LOS PRODUCTOS QUE TIENE EL CLIENTE CARGADOS EN EL LOCALSTORAGE
+    document.getElementById('idCarrito').innerText = JSON.parse(localStorage.listaProductosCarrito).length;
     
-    console.log(productoEnCanasto)
-    let mostrarProductoEnEsteDiv = document.getElementsByClassName('listaDeProductoPasarelaDePago');    
-    let divParaMostrastrProducto = document.createElement('div');
-    divParaMostrastrProducto.className = "card mb-3";
-    divParaMostrastrProducto.innerHTML =`<div class="card-body">
-                                            <div class="d-flex justify-content-between">
-                                                <div class="d-flex flex-row align-items-center">
-                                                    <div>
-                                                        <img src="../${productoEnCanasto.imagen}" class="img-fluid rounded-3 imgPago" alt="Shopping item">
+    let listaCarrito = JSON.parse(localStorage.getItem("listaProductosCarrito"))
+
+    for(let productoEnCanasto of listaCarrito){
+    
+        console.log(productoEnCanasto)
+        let mostrarProductoEnEsteDiv = document.getElementsByClassName('listaDeProductoPasarelaDePago');    
+        let divParaMostrastrProducto = document.createElement('div');
+        divParaMostrastrProducto.className = "card mb-3";
+        divParaMostrastrProducto.innerHTML =`<div class="card-body">
+                                                <div class="d-flex justify-content-between">
+                                                    <div class="d-flex flex-row align-items-center">
+                                                        <div>
+                                                            <img src="../${productoEnCanasto.imagen}" class="img-fluid rounded-3 imgPago" alt="Shopping item">
+                                                        </div>
+                                                        <div class="ms-3">
+                                                            <h5>${productoEnCanasto.nombreProducto}</h5>
+                                                            <p class="small mb-0">${productoEnCanasto.modeloProducto}</p>
+                                                        </div>
                                                     </div>
-                                                    <div class="ms-3">
-                                                        <h5>${productoEnCanasto.nombreProducto}</h5>
-                                                        <p class="small mb-0">${productoEnCanasto.modeloProducto}</p>
+                                                    <div class="d-flex flex-row align-items-center">
+                                                        <div>
+                                                            <h5 class="mb-0">$ ${productoEnCanasto.precioProducto}</h5>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="d-flex flex-row align-items-center">
-                                                    <div>
-                                                        <h5 class="mb-0">$ ${productoEnCanasto.precioProducto}</h5>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>`
-    mostrarProductoEnEsteDiv[0].appendChild(divParaMostrastrProducto);                                        
+                                            </div>`
+        mostrarProductoEnEsteDiv[0].appendChild(divParaMostrastrProducto);                                        
+    }
+}else{ //Si el cliente aún no tiene compras, se lo dirige al inicio.
+    swal({
+		title: "",
+		text: "¡Aún no haz realizado ninguna compra!",
+		type: "warning",
+		confirmButtonColor: "#D10024",
+		closeOnConfirm: false
+	  },
+	  function(isConfirm){
+		  if (isConfirm) {
+			  window.location.href = 'http://127.0.0.1:5501/index.html';
+		 	} 
+  });
 }
