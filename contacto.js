4cb4f8bc64f1d16ed315822aa3033ccb6a9fb112
Rafa Miranda <43553238+rafael749@users.noreply.github.com>
2022-05-26 13:57:41 -0300

    utilizacion de operadores ternarios
    utilizacion de operadores ternarios

--- contacto.js
+++ contacto.js
@@ -1,14 +1,5 @@
 
 //MUESTRO LOS PRODUCTOS QUE TIENE EL CLIENTE AÑADIDOS AL CARRITO CUANDO ESTOS PASAN AL LOCALSTORAGE
-if(localStorage.listaProductosDeseos != null){
-    //MUESTRO LOS PRODUCTOS QUE TIENE EL CLIENTE CARGADOS EN EL LOCALSTORAGE
-    document.getElementById('idCarrito').innerText = JSON.parse(localStorage.listaProductosCarrito).length;
-}else{
-    document.getElementById('idCarrito').innerText = 0;
-}
+(localStorage.listaProductosDeseos != null) ? document.getElementById('idCarrito').innerText = JSON.parse(localStorage.listaProductosCarrito).length : document.getElementById('idCarrito').innerText = 0;
 
-if(localStorage.listaProductosDeseos != null){
-document.getElementById('idDeseos').innerText = JSON.parse(localStorage.listaProductosDeseos).length;
-}else{
-    document.getElementById('idDeseos').innerText = 0;
-}
+(localStorage.listaProductosDeseos != null) ? document.getElementById('idDeseos').innerText = JSON.parse(localStorage.listaProductosDeseos).length : document.getElementById('idDeseos').innerText = 0;
