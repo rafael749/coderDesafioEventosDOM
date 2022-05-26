c9ae4fa2a3a6c18335a36136b2d3d5f7a23c783f
Rafa Miranda <43553238+rafael749@users.noreply.github.com>
2022-05-26 18:03:00 -0300

    operadores ternarios
    operadores ternarios

--- tecnologias.js
+++ tecnologias.js
@@ -1,18 +1,9 @@
 //MUESTRO LOS PRODUCTOS QUE TIENE EL CLIENTE AÑADIDOS AL CARRITO CUANDO ESTOS PASAN AL LOCALSTORAGE
-if(localStorage.listaProductosDeseos != null){
-    //MUESTRO LOS PRODUCTOS QUE TIENE EL CLIENTE CARGADOS EN EL LOCALSTORAGE
-    document.getElementById('idCarrito').innerText = JSON.parse(localStorage.listaProductosCarrito).length;
-}else{
-    document.getElementById('idCarrito').innerText = 0;
-}
+(localStorage.listaProductosDeseos != null) ? document.getElementById('idCarrito').innerText = JSON.parse(localStorage.listaProductosCarrito).length : document.getElementById('idCarrito').innerText = 0;
 
-if(localStorage.listaProductosDeseos != null){
-document.getElementById('idDeseos').innerText = JSON.parse(localStorage.listaProductosDeseos).length;
-}else{
-    document.getElementById('idDeseos').innerText = 0;
-}
+(localStorage.listaProductosDeseos != null) ? document.getElementById('idDeseos').innerText = JSON.parse(localStorage.listaProductosDeseos).length : document.getElementById('idDeseos').innerText = 0;
 
-let listaProductosAccesorios = JSON.parse(localStorage.getItem("productos"));
+let listaProductosAccesorios = JSON.parse(localStorage.getItem("productos")) || null;
 
     let contadorProd=0;
     for (const itemAccesorio of listaProductosAccesorios){
